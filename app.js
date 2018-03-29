ade8f24f8a5065d899a9d5e08f56bbbbab81ea36
rjdeleon <ricardopablo.deleon@gmail.com>
2018-03-29 18:17:12 +0800

    Add CORS support

--- app.js
+++ app.js
@@ -27,8 +27,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// add cross origin request support
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
